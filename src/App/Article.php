3840c372c388b2545dc8a86a32a380d798daa230
Uchirai <uchirai.govere@gmail.com>
2023-06-09 09:18:20 +0200

    using regex to clean up the title

--- src/App/Article.php
+++ src/App/Article.php
@@ -5,4 +5,17 @@ namespace App;
 class Article
 {
     public $title;
+
+    public function getSlug()
+    {
+        $slug = $this->title;
+
+        $slug = preg_replace('/[^\w]+/', '', $slug);
+
+        $slug = preg_replace('/\s+/', '_', $slug);
+
+        $slug = trim($slug, '_');
+
+        return $slug;
+    }
 }
--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -5,10 +5,39 @@ use App\Article;
 
 class ArticleTest extends TestCase
 {
+
+    public function setUp():void {
+        $this->article = new Article;
+    }
     public function testTitleIsEmptyByDefault()
     {
-        $article = new Article;
 
-        $this->assertEmpty($article->title);
+        $this->assertEmpty($this->article->title);
+    }
+
+    public function testSlugIsEmptyWithNoTitle()
+    {
+        $this->assertSame($this->article->getSlug(), '');
+    }
+
+    public function testSlugHasSpacesReplacedByUnderscores()
+    {
+        $this->article->title = 'An    example   article';
+
+        $this->assertSame($this->article->getSlug(), 'An_example_article');
+    }
+
+    public function testSlugDoesNotStartOrEndWithAnUnderscore()
+    {
+        $this->article->title = ' An example article ';
+
+        $this->assertSame($this->article->getSlug(), 'An_example_article');
+    }
+
+    public function testSlugDoesNotHaveAnyNonWordCharacters()
+    {
+        $this->article->title = 'Read! This! Now!';
+
+        $this->assertSame($this->article->getSlug(), 'Read_This_Now');
     }
 }
